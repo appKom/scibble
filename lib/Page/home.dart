62278112fbc5ac7f49a88ca4fabf7c75a51d4924
Siri Arnesen <siri.arnesen@ntnu.no>
2020-09-27 19:15:25 +0200

    Add hamburger menu. #5

--- lib/Page/home.dart
+++ lib/Page/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:scibble/Components/hamburger.dart';
 
 import 'package:scibble/OnlineWeb/Token.dart';
 import 'package:scibble/OnlineWeb/User.dart';
@@ -17,12 +18,14 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text('Scibble')),
       body: Column(
         children: [
           Text('Last name: ${widget.user.lastName}'),
           Text('Acess token: ${widget.token.accessToken}'),
         ],
       ),
+      drawer: HamburgerMenu(),
     );
   }
 }
