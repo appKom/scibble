b685d7d97a2dd5f9015e6518038db89a8a57553f
Paalar <paaledward@gmail.com>
2020-10-13 12:41:59 +0200

    remove unused imports

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_redux/flutter_redux.dart';
-import 'package:flutter_redux_navigation/flutter_redux_navigation.dart';
 
-import 'package:scibble/redux/store.dart';
 import 'package:scibble/widgets/hamburger.dart';
 import 'package:scibble/widgets/scibble_app_bar.dart';
 
